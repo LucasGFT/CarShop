8f065e662d868a6057d87225452962ab3240fcb5
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2023-03-22 14:29:39 -0300

    Iniciando Projeto

--- src/server.example.ts
+++ src/server.example.ts
@@ -1,6 +1,7 @@
 // import 'dotenv/config';
 // import app from './app';
 // import connectToDatabase from './Models/Connection';
+// salvar pullRequest
 
 // const PORT = process.env.PORT || 3001;
 // connectToDatabase()
